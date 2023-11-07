1a69e331e1e7955621e45395a7acd9c7be952729
absolutelyuniquer <1156326866@qq.com>
2023-11-07 18:49:36 +0800

    2023-11-07 18:49:36

--- core.py
+++ core.py
@@ -1,4 +1,10 @@
-import pandas as pd
 
-if __name__ == '__main__':
-  a = pd.
+            # 这是一个自动生成的Python文件
+            def hello_world():
+                print("Hello, world! Time is "Tue Nov  7 18:49:36 2023"")
+
+
+            if __name__ == "__main__":
+                hello_world()
+                b = 1
+            
